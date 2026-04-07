08aa2e78c41ff0ae63d8b77a77df7ce7b8b6562f
Ruchi Rajpoot <csjma21001164506@csjmu.ac.in>
2026-04-07 22:05:56 +0530

    Update Main.java

--- src/q02/Main.java
+++ src/q02/Main.java
@@ -1,10 +1,3 @@
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
         // TODO: Print Pascal's Triangle for n rows
         //       Values on each row separated by single space
         //       No leading or trailing spaces
@@ -16,6 +9,24 @@ public class Main {
         // 1 2 1
         // 1 3 3 1
         // 1 4 6 4 1
+import java.util.Scanner;
 
+public class PascalTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();  // Number of rows
+        sc.close();
+
+        for (int i = 0; i < n; i++) {
+            int val = 1;  // First element of each row is 1
+            for (int j = 0; j <= i; j++) {
+                System.out.print(val);
+                if (j < i) {
+                    System.out.print(" ");  // Separate values by single space
+                }
+                val = val * (i - j) / (j + 1);  // Compute next value in row
+            }
+            System.out.println();  // Move to next row
+        }
     }
 }

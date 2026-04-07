04898cd5f52583b46dbc929d5fe919cd02305c69
Ruchi Rajpoot <csjma21001164506@csjmu.ac.in>
2026-04-07 20:27:33 +0530

    Update Main.java

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -14,51 +14,52 @@
         //   * *
         //    *
 import java.util.Scanner;
-public class HollowDiamond {
+
+public class HollowDiamondPattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter half height (n): ");
         int n = sc.nextInt();
 
-        // Upper half (including middle line)
+        // Upper half (including middle)
         for (int i = 1; i <= n; i++) {
-            // Spaces
+
+            // Leading spaces
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
 
             // Stars and inner spaces
-            for (int j = 1; j <= (2 * i - 1); j++) {
-                if (j == 1 || j == (2 * i - 1)) {
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
                 }
             }
 
-            System.out.println();
+            System.out.println(); // no trailing spaces
         }
 
         // Lower half
         for (int i = n - 1; i >= 1; i--) {
-            // Spaces
+
+            // Leading spaces
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
 
             // Stars and inner spaces
-            for (int j = 1; j <= (2 * i - 1); j++) {
-                if (j == 1 || j == (2 * i - 1)) {
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
                 }
             }
 
-            System.out.println();
+            System.out.println(); // no trailing spaces
         }
 
         sc.close();
     }
 }
-

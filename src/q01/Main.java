e4b6a1aef703aa5c5e21f6a4b1e79ec976c5f8e4
Ruchi Rajpoot <csjma21001164506@csjmu.ac.in>
2026-04-07 20:37:00 +0530

    Update Main.java

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -20,46 +20,52 @@ public class HollowDiamondPattern {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        // Upper half (including middle)
+        // Upper half
         for (int i = 1; i <= n; i++) {
 
-            // Leading spaces
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
 
-            // Stars and inner spaces
-            for (int j = 1; j <= 2 * i - 1; j++) {
-                if (j == 1 || j == 2 * i - 1) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
+            if (i == 1) {
+                System.out.print("*");
+            } else {
+                for (int j = 1; j <= 2 * i - 1; j++) {
+                    if (j == 1 || j == 2 * i - 1) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
                 }
             }
 
-            System.out.println(); // no trailing spaces
+            System.out.println();
         }
 
         // Lower half
         for (int i = n - 1; i >= 1; i--) {
 
-            // Leading spaces
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
 
-            // Stars and inner spaces
-            for (int j = 1; j <= 2 * i - 1; j++) {
-                if (j == 1 || j == 2 * i - 1) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
+            if (i == 1) {
+                System.out.print("*");
+            } else {
+                for (int j = 1; j <= 2 * i - 1; j++) {
+                    if (j == 1 || j == 2 * i - 1) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
                 }
             }
 
-            System.out.println(); // no trailing spaces
+            System.out.println();
         }
 
         sc.close();
     }
 }
+
+     

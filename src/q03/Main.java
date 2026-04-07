73e9c11ba0a1862c06b1ebd35479ce41d481af87
Ruchi Rajpoot <csjma21001164506@csjmu.ac.in>
2026-04-08 00:11:11 +0530

    Update Main.java

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -1,9 +1,4 @@
-import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
         // TODO: Fill an N×N matrix in clockwise spiral order starting from 1
         //       Print each row with values separated by single space
@@ -16,5 +11,68 @@ public class Main {
         // 7 6 5
         // Diagonal: 15
 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[][] arr = new int[n][n];
+
+        int top = 0, bottom = n - 1;
+        int left = 0, right = n - 1;
+        int num = 1;
+
+        // Spiral fill
+        while (top <= bottom && left <= right) {
+
+            // Top row
+            for (int i = left; i <= right; i++) {
+                arr[top][i] = num++;
+            }
+            top++;
+
+            // Right column
+            for (int i = top; i <= bottom; i++) {
+                arr[i][right] = num++;
+            }
+            right--;
+
+            // Bottom row
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    arr[bottom][i] = num++;
+                }
+                bottom--;
+            }
+
+            // Left column
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    arr[i][left] = num++;
+                }
+                left++;
+            }
+        }
+
+        // Print matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j]);
+                if (j < n - 1) System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        // Diagonal sum
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i][i];
+        }
+
+        System.out.println("Diagonal: " + sum);
+
+        sc.close();
     }
-}
+             }

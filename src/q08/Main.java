ab0cb7dcd93317dedd8743b0c9f89974ab07b117
Ruchi Rajpoot <csjma21001164506@csjmu.ac.in>
2026-04-08 00:43:31 +0530

    Update Main.java

--- src/q08/Main.java
+++ src/q08/Main.java
@@ -1,11 +1,4 @@
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        // TODO: Implement a stack using an array (no Java Stack class)
+   // TODO: Implement a stack using an array (no Java Stack class)
         //       Process n operations:
         //         PUSH x  → push integer x
         //         POP     → pop top, print it; if empty print "EMPTY"
@@ -26,6 +19,49 @@ public class Main {
         // 20
         // 10
         // EMPTY
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        sc.nextLine(); // consume newline
+
+        int[] stack = new int[n];
+        int top = -1;
+
+        for (int i = 0; i < n; i++) {
+            String line = sc.nextLine();
+            String[] parts = line.split(" ");
+
+            String op = parts[0];
+
+            if (op.equals("PUSH")) {
+                int x = Integer.parseInt(parts[1]);
+                top++;
+                stack[top] = x;
+            } 
+            else if (op.equals("POP")) {
+                if (top == -1) {
+                    System.out.println("EMPTY");
+                } else {
+                    System.out.println(stack[top]);
+                    top--;
+                }
+            } 
+            else if (op.equals("PEEK")) {
+                if (top == -1) {
+                    System.out.println("EMPTY");
+                } else {
+                    System.out.println(stack[top]);
+                }
+            } 
+            else if (op.equals("SIZE")) {
+                System.out.println(top + 1);
+            }
+        }
+
+        sc.close();
     }
-}
+                }

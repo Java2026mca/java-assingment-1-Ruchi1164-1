94053b24a6ca535444460fb9d6983ec1cb82c24a
Ruchi Rajpoot <csjma21001164506@csjmu.ac.in>
2026-04-08 11:01:46 +0530

    matrix.java

--- src/q09/Main.java
+++ src/q09/Main.java
@@ -1,53 +1,39 @@
-    // TODO: Read two n×n matrices A and B, multiply them → print result C = A×B
-        //       Input: matrix A row by row, then matrix B row by row
-        //       Output: result matrix row by row, values separated by spaces
-        //
-        // Input:
-        // 2
-        // 1 2
-        // 3 4
-        // 5 6
-        // 7 8
-        //
-        // Output:
-        // 19 22
-        // 43 50
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
 
         int[][] A = new int[n][n];
         int[][] B = new int[n][n];
         int[][] C = new int[n][n];
 
-        // Input matrix A
+        // Read matrix A
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 A[i][j] = sc.nextInt();
             }
         }
 
-        // Input matrix B
+        
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 B[i][j] = sc.nextInt();
             }
         }
 
-        // Multiply A × B
+      
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
+                C[i][j] = 0;
                 for (int k = 0; k < n; k++) {
                     C[i][j] += A[i][k] * B[k][j];
                 }
             }
         }
 
-        // Print result
+      
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print(C[i][j]);
@@ -55,8 +41,5 @@ public class Main {
             }
             System.out.println();
         }
-
-        sc.close();
     }
-                 }
-    
+}
